740ce5e4e08d7378a557ddbce7566ae6547ec172
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-05-28 18:57:11 -0300

    :bug: fix: Requisito 22 - valor do discount corrigido pra 15

--- desafio22.sql
+++ desafio22.sql
@@ -1,3 +1,3 @@
 UPDATE order_details -- atualiza tabela
-SET discount = 30 -- configurando/modificando o conteúdo.
+SET discount = 15 -- configurando/modificando o conteúdo.
 WHERE unit_price < '10.0000'; -- onde vai essa atualização/configuração
