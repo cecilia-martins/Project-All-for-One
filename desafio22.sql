04454239ed2625a8b565929b7216aa42fb008b0f
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-05-28 18:50:58 -0300

    Adicionado requisito 23

--- desafio22.sql
+++ desafio22.sql
@@ -1,3 +1,3 @@
 UPDATE order_details -- atualiza tabela
-set discount = 30 -- configurando/modificando o conteúdo.
-where unit_price < '10.0000'; -- onde vai essa atualização/configuração
+SET discount = 30 -- configurando/modificando o conteúdo.
+WHERE unit_price < '10.0000'; -- onde vai essa atualização/configuração
